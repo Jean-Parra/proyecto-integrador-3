a5e3e51ed8385bb1739a540cc2777559ed946302
Miguel Mateo Mendoza Rojas <115849391+MiguelMRojas@users.noreply.github.com>
2023-02-18 19:55:06 -0500

    Update olvido.dart

--- lib/olvido.dart
+++ lib/olvido.dart
@@ -1,24 +1,66 @@
 import 'package:flutter/material.dart';
 
+import 'database/mongo.dart';
+
 class OlvidoPage extends StatefulWidget {
-  const OlvidoPage({super.key});
+  const OlvidoPage({Key? key}) : super(key: key);
 
   @override
-  // ignore: library_private_types_in_public_api
   _OlvidoPageState createState() => _OlvidoPageState();
 }
 
 class _OlvidoPageState extends State<OlvidoPage> {
+  final _formKey = GlobalKey<FormState>();
+  final _emailController = TextEditingController();
+  final _mongoDB = MongoDB();
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Olvido',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Olvido'),
-        ),
-        body: const Center(
-          child: Text('Vista del Olvido'),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Recuperar contraseña'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              const SizedBox(height: 16.0),
+              const Text(
+                'Ingresa tu correo electrónico',
+                style: TextStyle(fontSize: 16.0),
+              ),
+              TextFormField(
+                controller: _emailController,
+                keyboardType: TextInputType.emailAddress,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Por favor ingresa tu correo electrónico';
+                  }
+                  // TODO: validar que el correo existe en la base de datos
+                },
+              ),
+              const SizedBox(height: 32.0),
+              Center(
+                child: ElevatedButton(
+                  onPressed: () {
+                    if (_formKey.currentState!.validate()) {
+                      // TODO: enviar correo de recuperación de contraseña
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        const SnackBar(
+                          content: Text(
+                              'Se ha enviado un correo de recuperación de contraseña.'),
+                        ),
+                      );
+                    }
+                  },
+                  child: const Text('Recuperar contraseña'),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
