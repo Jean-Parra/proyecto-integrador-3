02b2de18241b8e0d59f1099dd60142404955cc3f
Jean-Parra <jean.parra.2020@upb.edu.co>
2023-03-09 00:04:41 -0500

    Docker

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const morgan = require('morgan');
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }))
+
+app.use(morgan('dev'));
 
 app.use(require('./controllers/authController'))
 
