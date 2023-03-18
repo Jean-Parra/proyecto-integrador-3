7a09d9de6b9ae3eec518dba1061d69414d802ccb
Jean-Parra <jean.parra.2020@upb.edu.co>
2023-03-18 04:31:01 -0500

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,11 @@
 const app = require('./app')
 require('./connection');
 
+const PORT = 80;
+
 async function init() {
-    await app.listen(80);
-    console.log('Servidor en Localhost:80')
+    await app.listen(PORT, '0.0.0.0');
+    console.log(`Servidor corriendo en http://upbcar.bucaramanga.upb.edu.co:${PORT}`);
 }
 
 init();
